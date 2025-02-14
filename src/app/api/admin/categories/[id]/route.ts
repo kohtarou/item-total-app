4bf84e887d9e6c5159ee28730a9294c014b56acf
koh8901 <120536905+kohtarou@users.noreply.github.com>
2025-02-14 18:41:52 +0900

    認証チェックのコードを削除し、APIエンドポイントの簡素化を図りました。

--- src/app/api/admin/categories/[id]/route.ts
+++ src/app/api/admin/categories/[id]/route.ts
@@ -18,16 +18,6 @@ export const PUT = async (req: NextRequest, routeParams: RouteParams) => {
     const id = routeParams.params.id;
     const { name }: RequestBody = await req.json();
 
-    // 認証チェック
-    const token = req.headers.get("Authorization") ?? "";
-    const { data, error } = await supabase.auth.getUser(token);
-    if (error || !data.user) {
-      return NextResponse.json(
-        { error: "認証に失敗しました" },
-        { status: 401 }
-      );
-    }
-
     const category: Category = await prisma.category.update({
       where: { id },
       data: { name },
@@ -45,17 +35,6 @@ export const PUT = async (req: NextRequest, routeParams: RouteParams) => {
 export const DELETE = async (req: NextRequest, routeParams: RouteParams) => {
   try {
     const id = routeParams.params.id;
-
-    // 認証チェック
-    const token = req.headers.get("Authorization") ?? "";
-    const { data, error } = await supabase.auth.getUser(token);
-    if (error || !data.user) {
-      console.error("認証エラー:", error);
-      return NextResponse.json(
-        { error: "認証に失敗しました" },
-        { status: 401 }
-      );
-    }
 
     const category = await prisma.category.delete({ where: { id } });
     return NextResponse.json({ msg: `「${category.name}」を削除しました。` });
--- src/app/api/admin/categories/route.ts
+++ src/app/api/admin/categories/route.ts
@@ -11,17 +11,6 @@ export const POST = async (req: NextRequest) => {
   try {
     const { name }: RequestBody = await req.json();
 
-    // 認証チェック
-    const token = req.headers.get("Authorization") ?? "";
-    const { data, error } = await supabase.auth.getUser(token);
-    if (error || !data.user) {
-      console.error("認証エラー:", error);
-      return NextResponse.json(
-        { error: "認証に失敗しました" },
-        { status: 401 }
-      );
-    }
-
     const category = await prisma.category.create({
       data: {
         name,
